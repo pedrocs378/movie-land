0a210b17895a210b400c621113265caebb271f52
Pedro César <pedrocs378@gmail.com>
2021-06-13 20:10:20 -0300

    chore: atualiza document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -34,6 +34,12 @@ export default class MyDocument extends Document {
 				<Head>
 					<link rel="preconnect" href="https://fonts.gstatic.com" />
 
+					<meta name="description" content="Procure por filmes e visualize suas informações. Adicione-o a sua própria lista de filmes." />
+					<meta itemProp="name" content="Movie Land" />
+					<meta property="og.title" content="Movie Land" />
+					<meta property="og.description" content="Procure por filmes e visualize suas informações. Adicione-o a sua própria lista de filmes." />
+					<meta property="og.url" content="https://movieland.dev/" />
+
 					<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
 					<link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400&display=swap" rel="stylesheet" />
 				</Head>
